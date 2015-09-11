5b5937a8e0572ba58f0624657e54511539228982
Jeuro <juuuro@gmail.com>
2015-09-11 15:34:15 +0300

    Implement ngram parser

---
+++ ngram_parser.py
@@ -0,0 +1,21 @@
+import csv
+import sys
+from collections import Counter
+
+filename = sys.argv[1]
+pattern = sys.argv[2]
+counter = Counter()
+
+with open(filename) as f:
+    for row in csv.reader(f, delimiter="\t"):
+        phrase = row[0]
+
+        if phrase.startswith(pattern):
+            last_word = phrase.split()[-1]
+            counter[last_word] += 1
+
+
+for word, count in counter.most_common():
+    print(word, count)
+
+
