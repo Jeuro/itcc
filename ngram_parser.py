013e8872cfc590016604410700603caa14c64567
Jeuro <juuuro@gmail.com>
2015-09-11 18:01:54 +0300

    Refactor

--- ngram_parser.py
+++ ngram_parser.py
@@ -3,38 +3,17 @@ import sys
 import re
 from collections import Counter
 
-filename = sys.argv[1]
-pattern = sys.argv[2]
 
-
-def get_noun(filename, word):
+def get_word(filename, regex, index):
     counter = Counter()
-    regex = "".join((r"as ", word, r" as [\S]* [\S]*$"))
     pattern = re.compile(regex)
 
     with open(filename) as f:
         for row in csv.reader(f, delimiter="\t"):
             phrase = row[0]
 
-            if pattern.match(phrase):
-                last_word = phrase.split()[-1]
-                counter[last_word] += int(row[2])
-
-    for word, count in counter.most_common():
-        print(word, count)
-
-
-def get_adjective(filename, word):
-    counter = Counter()
-    regex = r"as [\S]* as [\S]* "
-    pattern = re.compile("".join((regex, word, r"$")))
-
-    with open(filename) as f:
-        for row in csv.reader(f, delimiter="\t"):
-            phrase = row[0]
-
             if pattern.match(phrase):    
-                adjective = phrase.split()[1]                   
+                adjective = phrase.split()[index]                   
                 if counter[adjective] == 0:
                     print(phrase)             
                 counter[adjective] += int(row[2])
@@ -43,5 +22,16 @@ def get_adjective(filename, word):
         print(word, count)
 
 
-#get_adjective(filename, pattern)
-get_noun(filename, pattern)
+filename = sys.argv[1]
+pos = sys.argv[2]
+word = sys.argv[3]
+
+# query for adjectives
+if pos == "a":
+    regex = "".join((r"as [\S]* as [\S]* ", word, r"$"))
+    get_word(filename, regex, 1)
+
+# query for nouns
+if pos == "n":
+    regex = "".join((r"as ", word, r" as [\S]* [\S]*$"))
+    get_word(filename, regex, -1)
